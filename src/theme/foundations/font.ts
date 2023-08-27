d38a8d6ea20151f01dccca319f500bdd31b86848
Homer Bacanto <hmrbcnt@gmail.com>
2023-08-27 18:48:05 +0800

    feat: added foundations and typography mixins

---
+++ src/theme/foundations/font.ts
@@ -0,0 +1,5 @@
+export const font = {
+    primary: "'Plus Jakarta Sans', 'Arial'",
+};
+
+export type Font = typeof font;
---
+++ src/theme/mixins/typography.ts
@@ -0,0 +1,43 @@
+import { css } from "styled-components";
+
+const heading = {
+    headingXL: css`
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+    `,
+    headingL: css`
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+    `,
+    headingM: css`
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+    `,
+    headingS: css`
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        letter-spacing: 2.4px;
+    `
+};
+
+const body = {
+    medium: css`
+        font-size: 13px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 23px; /* 176.923% */
+    `
+};
+
+export const typography = {
+    heading,
+    body
+};
